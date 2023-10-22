e8f41a2b7d47b4b941dda6e3f26542b1e72d8b78
Ryuuukin <100138172+Ryuuukin@users.noreply.github.com>
2023-10-22 21:16:58 +0600

    Update KittenDormService.java

--- KittenDormService.java
+++ KittenDormService.java
@@ -5,13 +5,11 @@ public class KittenDormService {
     private Map<String, Object> kittenData = new HashMap<>();
 
     public KittenDormService() {
-        // Initialize with some initial data (for demonstration)
         kittenData.put("Fluffy", 2);
         kittenData.put("Whiskers", 3);
     }
 
     public Map<String, Object> fetchKittenData(String kittenId) {
-        // Retrieve kitten data by ID (in this case, kitten name)
         Map<String, Object> data = new HashMap<>();
         if (kittenData.containsKey(kittenId)) {
             data.put("KittenName", kittenId);
@@ -21,7 +19,6 @@ public class KittenDormService {
     }
 
     public void addFoundKitten(RawKittenData foundKitten) {
-        // Add found kitten to the service's data
         kittenData.put(foundKitten.getKittenName(), foundKitten.getKittenAge());
     }
 }
